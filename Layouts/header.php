b1df88274c85425b44efd80566a4a4f91d10708a
RaffiRmdhn <klsterbuka@gmail.com>
2025-05-09 10:14:50 +0700

    Adding ico

--- Layouts/header.php
+++ Layouts/header.php
@@ -2,6 +2,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?= $title ?> - Dashboard Admin</title>
 
+<!-- Favicons -->
+<link href="dist/logo.ico" rel="icon">
+<link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+
 <!-- Google Font: Source Sans Pro -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 <!-- Font Awesome -->
